e3447c230676596e97415a58b2694971a39c0842
Joaquin <joaquinglezsordo@gmail.com>
2022-11-03 09:11:51 +0100

    Creo la base de datos videoclub

---
+++ Videoclub.sql
@@ -0,0 +1,14 @@
+/*
+Autor: Joaquín González Sordo
+Fecha de inicio: 03-11-2022
+Proyecto: Base de datos de Videoclub
+*/
+
+drop database if exists Videoclub_Sardinero;
+CREATE DATABASE Videoclub_Sardinero;
+
+USE Videoclub_Sardinero;
+go
+
+
+
