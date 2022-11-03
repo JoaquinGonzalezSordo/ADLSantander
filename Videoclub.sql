9434d1f99eb566cff8d67994e9e57b852d83a4c5
JoaquinGonzalezSordo <joaquinglezsordo@gmail.com>
2022-11-03 10:34:01 +0100

    añado columna pais en tabla peliculas y continúo insertando registros

--- Videoclub.sql
+++ Videoclub.sql
@@ -16,6 +16,7 @@ CREATE TABLE Peliculas(
       Codpel char(2),
 	  titulo varchar(70) not null unique,
 	  genero varchar(20) not null,
+	  pais varchar(20),
 	  anio smallint,
 	  director varchar(40) not null,
 	  actorprincipal varchar(40),
@@ -28,17 +29,26 @@ CREATE TABLE Peliculas(
 
 
 INSERT INTO Peliculas
-   VALUES ('VL','Lo que el viento de llevó','Drama','1939',
+   VALUES ('VL','Lo que el viento de llevó','Drama','EEUU','1939',
            'Victor Fleming','Clark Gable','Vivien Leigh','8','Metro Goldwin Mayer');
 INSERT INTO Peliculas
-   VALUES ('CB','Casablanca','Drama','1942',
+   VALUES ('CB','Casablanca','Drama','EEUU','1942',
            'Michel Curtiz','Humphrey Bogart','Ingrid Bergman','3','Warner');
 INSERT INTO Peliculas
-   VALUES ('VZ','Viva Zapata','Drama','1952',
+   VALUES ('VZ','Viva Zapata','Drama','EEUU','1952',
            'Elia Kazan','Marlon Brando','Jean Peters','1','20 th centiuries');
 INSERT INTO Peliculas
-   VALUES ('BN','Blancanieves','Infantil','1937',
+   VALUES ('BN','Blancanieves','Infantil','EEUU','1937',
            'Walt Disney','El principe','Blancanieves','1','Disney');
+INSERT INTO Peliculas
+   VALUES ('BH','Ben Hur','Drama','EEUU','1959',
+           'William Wyler','Charlton Heston','','11','Metro Goldwin Mayer');
+INSERT INTO Peliculas
+   VALUES ('WS','West side story','musical','EEUU','1961',
+           'Robert Wise','Richard Beymer','Natalie Wood','10','Seven Arts');
+		   INSERT INTO Peliculas
+   VALUES ('LA','Lawrence de Arabia','historica','Reino Unido','1962',
+           'David Lean','Peter O Toole','','7','Columbia');
 
 select * from Peliculas
 
